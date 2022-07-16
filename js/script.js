96b51ebe8b6e5985452f6d7512abaeb0b4956417
lazoliver <wilian.oliveiractt@gmail.com>
2022-07-16 18:29:54 -0300

    dropdown-icon: toggleClass

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
     $("#menu-btn").click(function() {
         $("nav").css({"transition": "2s ease all", "margin-top": "0"});
     });
+    // Menu Fechar
     $("#close-btn").click(function() {
         $("nav").css({"transition": "2s ease all", "margin-top": "-220%"});
     });
     // Dropdown - Soluções
     $("#solucoes").click(function() {
         $("#dropdown-solucoes").toggle("fast");
+        $("#solucoes .close-icon-top").toggleClass("dropdown-icon");
     });
     // Dropdown - Treinamentos
     $("#treinamentos").click(function() {
         $("#dropdown-treinamentos").toggle("fast");
+        $("#treinamentos .close-icon-top").toggleClass("dropdown-icon");
     });
 });
